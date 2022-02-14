e3ed1fc1ef0317b52dd87d0e38ed3b6d9741fa11
najmul <najmulhasanm677@gmail.com>
2022-02-14 10:36:22 +0600

    firebase api changed

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const app = initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER,
-    appId: process.env.REACT_APP_ID,
+    apiKey: 'AIzaSyAPFpV4GmDb_FAL4ZR1iNlKQ1BEPji1_64',
+    authDomain: 'corporate-app-872e5.firebaseapp.com',
+    projectId: 'corporate-app-872e5',
+    storageBucket: 'corporate-app-872e5.appspot.com',
+    messagingSenderId: '276109329505',
+    appId: '1:276109329505:web:6c5366813610ad841d1620',
 });
 
 export default app;
